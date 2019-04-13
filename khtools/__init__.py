4747eb472baef808146e3210c7c99549b05baf16
Olga Botvinnik <olga.botvinnik@gmail.com>
2019-04-13 10:45:37 -0700

    Add all files as submodules

--- khtools/__init__.py
+++ khtools/__init__.py
@@ -3,3 +3,16 @@
 __author__ = 'Olga Botvinnik'
 __email__ = 'olga.botvinnik@czbiohub.org'
 __version__ = '0.1.0'
+
+
+from . import common
+from . import downsample_hashes
+from . import extract_metadata
+from . import idf
+from . import jaccard_utils
+from . import jupyter_utils
+from . import knn
+from . import os_utils
+from . import s3_utils
+from . import sourmash_compare_utils
+from . import sourmash_utils
