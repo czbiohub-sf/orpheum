d7383edbf23fd53889682934ca3c6f3a4973fc32
Olga Botvinnik <olga.botvinnik@gmail.com>
2019-04-13 11:29:31 -0700

    fix relative import

--- khtools/jaccard_utils.py
+++ khtools/jaccard_utils.py
@@ -2,13 +2,19 @@
 from joblib import Parallel, delayed
 import itertools
 
+
+from .idf import filter_idf
+
 def jaccard_sigs(i, j, siglist):
     return siglist[i].jaccard(siglist[j])
 
+
 def jaccard_sigs_idf(i, j, siglist, idf, mean_idf_per_cell):
-    i_hashes = filter_idf(siglist[i].get_mins(), inverse_document_frequency, mean_idf_per_cell)
-    j_hashes = filter_idf(siglist[j].get_mins(), inverse_document_frequency, mean_idf_per_cell)
-    return jacard(i_hashes, j_hashes)
+    i_hashes = filter_idf(siglist[i].get_mins(),
+                          idf, mean_idf_per_cell)
+    j_hashes = filter_idf(siglist[j].get_mins(),
+                          idf, mean_idf_per_cell)
+    return jaccard(i_hashes, j_hashes)
 
 
 def jaccard(sample1, sample2):
