9e385681c279ac2be67e6fd34df6615183fdf573
Olga Botvinnik <olga.botvinnik@gmail.com>
2019-04-15 19:39:15 -0700

    Figures are bytes to write!

--- khtools/s3_utils.py
+++ khtools/s3_utils.py
@@ -18,7 +18,7 @@ def savefig(fig, filename, **kwargs):
     """Saves figure to s3 if path starts with s3:// otherwise saves locally"""
     if filename.startswith('s3://'):
         fs = s3fs.S3FileSystem(anon=False)
-        with fs.open(filename, 'w') as f:
+        with fs.open(filename, 'wb') as f:
             return fig.savefig(f, **kwargs)
     else:
         return fig.savefig(filename)
