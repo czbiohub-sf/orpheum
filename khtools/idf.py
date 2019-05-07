8a7e4d0f3200e39a4c2a718e91ffa9da09206b52
Olga Botvinnik <olga.botvinnik@gmail.com>
2019-05-07 10:25:51 -0700

    Fix merge conflict

--- khtools/idf.py
+++ khtools/idf.py
@@ -1,5 +1,5 @@
 
-from collections import Counter
+from collections import Counter, defaultdict
 import math
 
 
@@ -42,3 +42,39 @@ def get_inverse_document_frequency(siglist):
     document_frequency = get_document_frequency(siglist)
     inverse_document_frequency = {k: math.log(N/v) for k, v in document_frequency.items()}
     return inverse_document_frequency
+
+
+def make_siglist_groups(siglist, groups):
+    """Map groups names in series to lists of signatures
+
+    Returns
+    -------
+    siglist_grouped : dict
+        dictionary of string name to a list of signatures
+    """
+    groups_siglist = [groups[x.name()] for x in siglist]
+
+    siglist_grouped = defaultdict(list)
+    for animal, sig in zip(groups_siglist, siglist):
+        siglist_grouped[animal].append(sig)
+    return siglist_grouped
+
+
+def perform_tf_idf(siglist, groups):
+    siglist_grouped = make_siglist_groups(siglist, groups)
+
+    siglist_grouped_tf_idf = {}
+
+    siglist_species_tf_idf = []
+
+    for species, species_sigs in siglist_grouped.items():
+        species_sigs = list(species_sigs)
+        species_idf = get_inverse_document_frequency(species_sigs)
+        species_mean_idf_per_cell = idf.get_mean_idf_per_cell(species_sigs,
+                                                              species_idf)
+        species_sigs_filtered_idf = [idf.filter_idf(x.minhash.get_mins(),
+                                                    species_idf,
+                                                    species_mean_idf_per_cell)
+                                     for x in species_sigs]
+        siglist_grouped_tf_idf[species] = species_sigs_filtered_idf
+        siglist_species_tf_idf.extend(species_sigs_filtered_idf)
--- khtools/jaccard_utils.py
+++ khtools/jaccard_utils.py
@@ -1,6 +1,8 @@
 
 from joblib import Parallel, delayed
 import itertools
+from collections import defaultdict
+
 
 
 from .idf import filter_idf
@@ -8,12 +10,9 @@ from .idf import filter_idf
 def jaccard_sigs(i, j, siglist):
     return siglist[i].jaccard(siglist[j])
 
-
-def jaccard_sigs_idf(i, j, siglist, idf, mean_idf_per_cell, ):
-    i_hashes = filter_idf(siglist[i].get_mins(),
-                          idf, mean_idf_per_cell)
-    j_hashes = filter_idf(siglist[j].get_mins(),
-                          idf, mean_idf_per_cell)
+def jaccard_sigs_idf(i, j, siglist, idf, mean_idf_per_cell):
+    i_hashes = filter_idf(siglist[i].get_mins(), inverse_document_frequency, mean_idf_per_cell)
+    j_hashes = filter_idf(siglist[j].get_mins(), inverse_document_frequency, mean_idf_per_cell)
     return jaccard(i_hashes, j_hashes)
 
 
@@ -22,3 +21,33 @@ def jaccard(sample1, sample2):
     intersection = len(sample1.intersection(sample2))
     union = len(sample1.union(sample2))
     return intersection/union
+
+
+def estimate_jaccard(a, b, normalize=True):
+    """Estimate jaccard similarity between A and B.
+
+    Normalizes A and B to be the same size, and takes the sketch of A U B
+    """
+    min_length = min(len(a), len(b))
+
+    # Renormalize the sketch lengths to be identical
+    if normalize:
+        a = set(sorted(a)[:min_length])
+        b = set(sorted(b)[:min_length])
+
+    sketch_a_union_b = set(sorted(a.union(b))[:min_length])
+
+    numerator = len(sketch_a_union_b.intersection(a).intersection(b))
+    denominator = len(sketch_a_union_b)
+    return numerator / denominator
+
+
+
+
+
+def jaccard_tf_idf(siglist, series):
+    series_groups = [series[x.name()] for x in siglist]
+
+    siglist_grouped = defaultdict(list)
+    for animal, sig in zip(series_groups, siglist):
+        siglist_grouped[animal].append(sig)
