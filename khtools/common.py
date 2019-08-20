75e86ab0e11f82644960263a7ca2bc5b2f48dcbb
Olga Botvinnik <olga.botvinnik@gmail.com>
2019-08-20 16:33:52 -0700

    Add loading tabula muris annotations

--- khtools/common.py
+++ khtools/common.py
@@ -1,5 +1,6 @@
 import os
 
+import pandas as pd
 
 from . import jupyter_utils
 
@@ -17,3 +18,17 @@ def get_figure_folder():
     notebook_basename = get_notebook_basename()
     figure_folder = os.path.join(FIGURE_FOLDER, notebook_basename)
     return figure_folder
+
+
+def load_tabula_muris_annotations():
+    """Fetch latest FACS annotations of Tabula Muris data from GitHub"""
+    annotations = pd.read_csv(
+        'https://github.com/czbiohub/tabula-muris/raw/master/00_data_ingest/18_global_annotation_csv/annotations_facs.csv',
+        index_col='cell')
+
+    # Sanitize the input - no dots
+    annotations.index = annotations.index.str.replace('.', '-')
+    annotations.columns = annotations.columns.str.replace('.', '_')
+    annotations['sample_id'] = annotations.index
+    annotations = annotations.fillna("NA")
+    return annotations
