50d5be59b06b486fb5ea360152c2bd2985fecd63
Olga Botvinnik <olga.botvinnik@gmail.com>
2019-08-30 14:37:41 -0700

    Whitespaceˆ

--- khtools/s3_utils.py
+++ khtools/s3_utils.py
@@ -12,10 +12,12 @@ def write_s3(df, filename, fmt='csv', **kwargs):
         # Parquet is a binary format and needs the "b" flag
         with fs.open(filename, 'wb') as f:
             return df.to_parquet(f, **kwargs)
-        
+
+
 def read_aws_s3_ls(filename, **kwargs):
     return pd.read_table(filename, delim_whitespace=True, header=None, 
                           names=['date', 'time', 'bytes', 'basename'], **kwargs)
+
 
 def savefig(fig, filename, **kwargs):
     """Saves figure to s3 if path starts with s3:// otherwise saves locally"""
