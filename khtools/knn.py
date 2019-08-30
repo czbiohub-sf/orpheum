7a3c00d36b98309e4f57922ead068d883102b4c4
Olga Botvinnik <olga.botvinnik@gmail.com>
2019-08-30 14:52:24 -0700

    Add savefig import

--- khtools/knn.py
+++ khtools/knn.py
@@ -7,6 +7,8 @@ import pandas as pd
 from sklearn.neighbors import NearestNeighbors
 
 from . import sourmash_utils
+from .s3_utils import savefig
+
 
 def _compute_neighbor_adjacencies(data, n_neighbors=5):
     # Convert to distances by subtracting from 1
