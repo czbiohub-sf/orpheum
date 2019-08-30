1994cc7b1a4fea8f42d560401bf68b37a6e5198d
Olga Botvinnik <olga.botvinnik@gmail.com>
2019-08-30 14:36:50 -0700

    Add savefig command

--- khtools/s3_utils.py
+++ khtools/s3_utils.py
@@ -16,3 +16,12 @@ def write_s3(df, filename, fmt='csv', **kwargs):
 def read_aws_s3_ls(filename, **kwargs):
     return pd.read_table(filename, delim_whitespace=True, header=None, 
                           names=['date', 'time', 'bytes', 'basename'], **kwargs)
+
+def savefig(fig, filename, **kwargs):
+    """Saves figure to s3 if path starts with s3:// otherwise saves locally"""
+    if filename.startswith('s3://'):
+        fs = s3fs.S3FileSystem(anon=False)
+        with fs.open(filename, 'wb') as f:
+            return fig.savefig(f, **kwargs)
+    else:
+        return fig.savefig(filename)
