91777ba6c5da782c1e8661b1719a12205fb38c06
Olga Botvinnik <olga.botvinnik@gmail.com>
2019-10-11 09:57:51 -0700

    Separate out kmer string making

--- khtools/partition_reads.py
+++ khtools/partition_reads.py
@@ -121,7 +121,8 @@ def score_single_translation(translation, peptide_graph, peptide_ksize,
                            zip(kmers, hashes)}
     if verbose:
         # Print keys (kmers) only
-        print(f"K-mers in peptide database: {', '.join(kmers_in_peptide_db.keys())}")
+        kmer_string = ', '.join(kmers_in_peptide_db.keys())
+        print(f"K-mers in peptide database: {kmer_string}")
     fraction_in_peptide_db = n_kmers_in_peptide_db / n_kmers
     return fraction_in_peptide_db, n_kmers
 
