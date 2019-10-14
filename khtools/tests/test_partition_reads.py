d508350dbf491633b68e1f65e9d58f97e198ff50
Olga Botvinnik <olga.botvinnik@gmail.com>
2019-10-13 23:45:59 -0700

    start writing tests for partitioning reads

--- khtools/tests/test_partition_reads.py
+++ khtools/tests/test_partition_reads.py
@@ -1,10 +1,13 @@
+from io import StringIO
 import os
 import warnings
 
-
 from Bio.Seq import Seq
+from click.testing import CliRunner
+from khmer import Nodegraph
+import pandas as pd
+import pandas.util.testing as pdt
 import pytest
-
 
 
 @pytest.fixture
@@ -49,3 +52,53 @@ def test_six_frame_translation_no_stops(seq):
             -3: 'QNRIYSYIAIFLILLMSVLSK'}
     assert test == true
 
+
+@pytest.fixture
+def peptide_graph(data_folder):
+    filename = os.path.join(data_folder, 'bloom_filter',
+                            'Homo_sapiens.GRCh38.pep.subset.molecule-protein_ksize-7.bloomfilter.nodegraph')
+    return Nodegraph.load(filename)
+
+
+@pytest.fixture
+def reads(data_folder):
+    return os.path.join(data_folder,
+                        'SRR306838_GSM752691_hsa_br_F_1_trimmed_subsampled_first20.fq.gz')
+
+
+def test_score_reads(reads, peptide_graph):
+    from khtools.partition_reads import score_reads
+
+    test = score_reads(reads, peptide_graph, peptide_ksize=7,
+                                jaccard_threshold=0.9, molecule='protein')
+    s = '''read_id,jaccard_in_peptide_db,n_kmers,classification
+SRR306838.10559374 Ibis_Run100924_C3PO:6:51:17601:17119/1,1.0,16,coding
+SRR306838.6196593 Ibis_Run100924_C3PO:6:29:16733:12435/1,0.0,17,non-coding
+SRR306838.20767303 Ibis_Run100924_C3PO:6:104:6864:5062/1,0.0,16,non-coding
+SRR306838.12582274 Ibis_Run100924_C3PO:6:62:11779:17975/1,0.0,16,non-coding
+SRR306838.13334230 Ibis_Run100924_C3PO:6:66:16579:20350/1,0.0,12,non-coding
+SRR306838.2740879 Ibis_Run100924_C3PO:6:13:11155:5248/1,1.0,18,coding
+SRR306838.6813354 Ibis_Run100924_C3PO:6:32:10591:13073/1,0.0,17,non-coding
+SRR306838.23113368 Ibis_Run100924_C3PO:6:114:13840:18459/1,0.0,14,non-coding
+SRR306838.10872941 Ibis_Run100924_C3PO:6:53:6164:10522/1,0.0,16,non-coding
+SRR306838.6192120 Ibis_Run100924_C3PO:6:29:5833:11991/1,0.0,16,non-coding
+SRR306838.21295280 Ibis_Run100924_C3PO:6:106:2590:13965/1,0.0,17,non-coding
+SRR306838.21201208 Ibis_Run100924_C3PO:6:106:2763:5109/1,0.0,0,non-coding
+SRR306838.18327923 Ibis_Run100924_C3PO:6:92:9077:13885/1,0.0,16,non-coding
+SRR306838.4880582 Ibis_Run100924_C3PO:6:23:17413:5436/1,1.0,16,coding
+SRR306838.21417895 Ibis_Run100924_C3PO:6:107:8793:5012/1,0.0,16,non-coding
+SRR306838.17165743 Ibis_Run100924_C3PO:6:86:18789:18450/1,0.0,0,non-coding
+SRR306838.21229494 Ibis_Run100924_C3PO:6:106:6163:7753/1,0.0,0,non-coding
+SRR306838.21218773 Ibis_Run100924_C3PO:6:106:16921:6743/1,0.0,5,non-coding
+SRR306838.20124664 Ibis_Run100924_C3PO:6:101:4701:5309/1,0.05555555555555555,18,non-coding
+SRR306838.16841308 Ibis_Run100924_C3PO:6:85:6205:5805/1,0.0,17,non-coding'''
+    true = pd.read_csv(StringIO(s))
+    pdt.assert_equal(test, true)
+
+
+def test_cli():
+
+    runner = CliRunner()
+    result = runner.invoke(hello, ['Peter'])
+    assert result.exit_code == 0
+    assert result.output == 'Hello Peter!\n'
