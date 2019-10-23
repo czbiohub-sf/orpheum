d32a495a9d7a58caa8ee932f5552bff60e9fd6fc
Olga Botvinnik <olga.botvinnik@gmail.com>
2019-10-22 17:51:05 -0700

    Use a smaller table size for testing

--- khtools/tests/test_bloom_filter.py
+++ khtools/tests/test_bloom_filter.py
@@ -5,18 +5,15 @@ def test_make_peptide_bloom_filter(peptide_fasta, molecule, peptide_ksize):
     from khtools.bloom_filter import make_peptide_bloom_filter
 
     test = make_peptide_bloom_filter(peptide_fasta, peptide_ksize, molecule,
-                                     tablesize=1e6)
-    TRUE_N_UNIQUE_KMERS = {("protein", 7): 506352,
-                           ("dayhoff", 7): 99863,
-                           ("dayhoff", 12): 488469,
-                           ("hydrophobic-polar", 21): 434810,
+                                     tablesize=1e4)
+    TRUE_N_UNIQUE_KMERS = {("protein", 7): 20717,
+                           ("dayhoff", 7): 20669,
+                           ("dayhoff", 12): 20712,
+                           ("hydrophobic-polar", 21): 20712,
                            ("hydrophobic-polar", 7): 170}
     true_n_unique_kmers = TRUE_N_UNIQUE_KMERS[(molecule, peptide_ksize)]
 
     # For now, assert that the number of kmers is within 5% of the true value
     assert test.n_unique_kmers() > true_n_unique_kmers * 0.95
     assert test.n_unique_kmers() < true_n_unique_kmers * 1.05
-    base_dir = '/Users/olgabot/code/kmer-hashing/kh-tools/khtools/tests/data/bloom_filter'
-    filename = f'{base_dir}/Homo_sapiens.GRCh38.pep.subset.molecule-{molecule}_ksize-{peptide_ksize}.bloomfilter.nodegraph'
-    test.save(filename)
 
