be46cd99d4eee39ed52f09349952f63484c0897e
Olga Botvinnik <olga.botvinnik@gmail.com>
2019-11-08 08:52:36 -0800

    Add failing test of stdout to fix later

--- khtools/tests/test_homology.py
+++ khtools/tests/test_homology.py
@@ -22,19 +22,31 @@ def homologues_tsv_gz(data_folder):
     return os.path.join(data_folder, 'homology',
                         'human-chicken-orthologs-subset.tsv.gz')
 
+@pytest.fixture
+def homology_scores_allpairs_csv(data_folder):
+    return os.path.join(data_folder, 'homology',
+                        'human-chicken-orthologs-subset-kmer-homology-scores'
+                        '__allpairs_nbackground2.csv')
+
+
+@pytest.fixture
+def homology_scores_allpairs(homology_scores_allpairs_csv):
+    return pd.read_csv(homology_scores_allpairs_csv)
+
 #
 # class TestHomologyTable:
 #
 #     pass
 
 
-def test_cli_tsv_all_pairs(homologues_tsv):
+def test_cli_tsv_all_pairs(homologues_tsv, homology_scores_allpairs):
     from khtools.homology import cli
 
     runner = CliRunner()
     result = runner.invoke(cli, ['--n-background', "2",
                                  "human", "chicken", homologues_tsv])
     assert result.exit_code == 0
+    assert homology_scores_allpairs.to_csv() == result.stdout
 
 
 def test_cli_tsv_n_subset5(homologues_tsv):
