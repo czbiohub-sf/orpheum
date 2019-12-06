3a87b76efec749c2f1728861ca4a22d92d5f03aa
Olga Botvinnik <olga.botvinnik@gmail.com>
2019-12-06 05:41:47 -0800

    Start writing SimilarityMatrix class

--- khtools/similarity_matrix.py
+++ khtools/similarity_matrix.py
@@ -1,13 +1,21 @@
+from anndata import Anndata
 import pandas as pd
 
 
-class SimilarityMatrix:
+class SimilarityMatrix(Anndata):
 
-    def __init__(self):
-        pass
+    def __init__(self, data, metadata=None):
+        self.data = data
+        self.metadata = metadata
+
+    def _validate_metadata(self):
+        # Check that all metadata
+        assert
+
 
     @classmethod
     def from_csv(csv):
-        similarities = pd.read_csv(pd)
+        similarities = pd.read_csv(csv)
         similarities.index = similarities.columns
-        return SimilarityMatrix(similarities)
+        return SimilarityMatrix(similarities, filename=csv)
+
