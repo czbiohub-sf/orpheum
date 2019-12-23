820f32940016faee11e91d61075408bf3ed9da6a
Olga Botvinnik <olga.botvinnik@gmail.com>
2019-12-22 21:05:20 -0800

    Check out sequence_encodings from olgabot/more-encodings

--- khtools/sequence_encodings.py
+++ khtools/sequence_encodings.py
@@ -317,7 +317,7 @@ DAYHOFF_LIKE = 'dayhoff', 'dayhoff6'
 HP_LIKE = 'hydrophobic-polar', 'hydrophobic-polar2', 'hp', 'hp2',
 
 VALID_PEPTIDE_MOLECULES = 'protein', 'peptide', \
-                          'protein20', 'peptide20'\
+                          'protein20', 'peptide20', \
                           'aa20', \
                           'dayhoff', 'dayhoff6' \
                           'botvinnik', 'botvinnik8', \
